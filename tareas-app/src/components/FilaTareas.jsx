13154b0092eeade15f87cdc05404246203196e2e
unknown <egusquizacristina@gmail.com>
2025-05-06 11:38:23 +0200

    quito la funcion de calcular el tiempo restante en FilaTareas.jsx y TablaTareas.jsx

--- tareas-app/src/components/FilaTareas.jsx
+++ tareas-app/src/components/FilaTareas.jsx
@@ -15,39 +15,8 @@ const FilaTarea = ({ tarea, onBorrar }) => {
     }
   };
 
-  // Función para calcular el tiempo restante
-  const calcularTiempoRestante = (momento) => {
-    const fechaTarea = new Date(momento);
-    const fechaActual = new Date();
-    const diferencia = fechaTarea - fechaActual;
-
-    if (diferencia <= 0) {
-      return "La tarea ya venció.";
-    }
-
-    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
-    const horas = Math.floor(
-      (diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
-
-    return `${dias} días, ${horas} horas y ${minutos} minutos restantes.`;
-  };
-
-  // Función para manejar el evento onMouseOver
-  const handleMouseOver = () => {
-    if (tarea.momento) {
-      alert(calcularTiempoRestante(tarea.momento));
-    } else {
-      alert("No se especificó una fecha para esta tarea.");
-    }
-  };
-
   return (
-    <tr
-      className={obtenerClasePrioridad(tarea.prioridad)}
-      onMouseOver={handleMouseOver} // Asignar la función al evento onMouseOver
-    >
+    <tr className={obtenerClasePrioridad(tarea.prioridad)}>
       <td>{tarea.texto}</td>
       <td>{tarea.prioridad}</td>
       <td>{tarea.momento || "No especificada"}</td>
